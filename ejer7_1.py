4984a92600ed908d82b43318047799f3ba4f191c
lucber140405 <122527959+lucber140405@users.noreply.github.com>
2023-02-10 09:57:34 +0100

    Add files via upload

---
+++ ejer7_1.py
@@ -0,0 +1,4 @@
+name= input("dime un nombre ")
+number = int(input("dime un numero"))
+for i in range(number):
+    print (name)
---
+++ ejer7_3.py
@@ -0,0 +1,5 @@
+numero= int(input("dime un numero entero"))
+for i in range (1,numero+1):
+    if i%7==0:
+        print(i)
+   
